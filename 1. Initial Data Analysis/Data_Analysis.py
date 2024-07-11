a35e470c50fe5284b9cba255cb1585a7af0e5354
RickySallan <ricky.sallan@gmail.com>
2024-07-11 13:41:44 +0000

    Data_Analysis.py - Load CSV, high level data analysis which created output files (df_shape_output.txt, df_info_output.txt, df_head_output.txt) to give a detail of the dataset

--- 1. Initial Data Analysis/Data_Analysis.py
+++ 1. Initial Data Analysis/Data_Analysis.py
@@ -3,7 +3,7 @@
 ##############################################################################################################################################
 
 ##############################################################################################################################################
-# LOAD THE DATASET
+# 1. LOAD THE DATASET
 
 import pandas as pd
 
@@ -14,14 +14,20 @@ df = pd.read_csv(file_path)
 ##############################################################################################################################################
 
 ##############################################################################################################################################
-# ANALYSIS OF THE DATASET
+# 2. ANALYSIS OF THE DATASET
+
+import os
+
+output_folder='/workspaces/PAIWP_ASSIGNMENT/1. Initial Data Analysis'
 
 # df.shape - How many rows and columns does the dataset have? -- I have also used this function to test if the file has been loaded successfully
 df_shape=df.shape
 
 file_name_shape = "df_shape_output.txt"
 
-with open(file_name_shape,'w') as file:
+full_file_path_shape=os.path.join(output_folder,file_name_shape)
+
+with open(full_file_path_shape,'w') as file:
     file.write(f"Number of rows: {df_shape[0]}\n")
     file.write(f"Number of columns: {df_shape[1]}\n")
 
@@ -34,13 +40,18 @@ info_str=buffer.getvalue()
 
 file_name_info='df_info_output.txt'
 
-with open(file_name_info,'w') as f:
+full_file_path_info=os.path.join(output_folder,file_name_info)
+
+with open(full_file_path_info,'w') as f:
     f.write(info_str)
 
 # df.head - What does a small sample of the data look like? -- I have also used this function to test if the file has been loaded successfully
 df_head_str=df.head(10).to_string(index=False)
 
-file_path="df_head_output.txt"
-with open(file_path, 'w') as f:
+file_path_head="df_head_output.txt"
+
+full_file_path_head=os.path.join(output_folder,file_path_head)
+
+with open(full_file_path_head, 'w') as f:
     f.write(df_head_str)
 ##############################################################################################################################################
