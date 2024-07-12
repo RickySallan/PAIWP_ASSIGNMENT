c3204287a80215c3806271763efa8aed7ed73f6c
RickySallan <ricky.sallan@gmail.com>
2024-07-12 12:33:45 +0000

    Encoding

--- 3. Data Cleaning/Data_Cleaning.py
+++ 3. Data Cleaning/Data_Cleaning.py
@@ -19,7 +19,7 @@ df = pd.read_csv(file_path)
 
 columns_to_drop = ['Customer ID','Referred a Friend','Number of Referrals','Offer','Phone Service','Avg Monthly Long Distance Charges','Multiple Lines','Internet Service','Internet Type','Avg Monthly GB Download','Online Security','Online Backup','Device Protection Plan','Premium Tech Support','Streaming TV','Streaming Movies','Streaming Music','Unlimited Data','Paperless Billing','Payment Method','Monthly Charge','Total Regular Charges','Total Refunds','Total Extra Data Charges','Total Long Distance Charges','Gender','Age','Under 30','Senior Citizen','Married','Dependents','Number of Dependents','City','Zip Code','Latitude','Longitude','Population','CLTV']
 
-df_cleaned=df.drop(columns=columns_to_drop)
+df_cleaned=df.drop(columns=columns_to_drop).fillna(0)
 ##############################################################################################################################################
 
 ##############################################################################################################################################
@@ -55,7 +55,7 @@ with open(full_file_path_info,'w') as f:
     f.write(info_str)
 
 # df_cleaned.head - What does a small sample of the data look like? -- I have also used this function to test if the file has been loaded successfully
-df_head_str=df_cleaned.head(10).to_string(index=False)
+df_head_str=df_cleaned.head(500).to_string(index=False)
 
 file_path_head="df_cleaned_head_output.txt"
 
@@ -64,3 +64,46 @@ full_file_path_head=os.path.join(output_folder,file_path_head)
 with open(full_file_path_head, 'w') as f:
     f.write(df_head_str)
 ##############################################################################################################################################
+# 4. Data Preprocessing
+
+# Convert categorical data to numerical data using one-hot encoding
+categorical_cols = ['Contract', 'Churn Category', 'Churn Reason']
+df_encoded = pd.get_dummies(df_cleaned, columns=categorical_cols)
+
+# df_encoded.shape - How many rows and columns does the dataset have? -- I have also used this function to test if the file has been loaded successfully
+df_encoded_shape=df_encoded.shape
+
+file_name_encoded_shape = "df_encoded_shape_output.txt"
+
+full_file_path_encoded_shape=os.path.join(output_folder,file_name_encoded_shape)
+
+with open(full_file_path_encoded_shape,'w') as file:
+    file.write(f"Number of rows: {df_encoded_shape[0]}\n")
+    file.write(f"Number of columns: {df_encoded_shape[1]}\n")
+
+# df_encoded.info - Print a summary of the Dataset to identify the data type of each column and non-null values -- This will assist with identifying which columns need to be cleaned
+from io import StringIO
+
+buffer=StringIO()
+df_encoded.info(buf=buffer)
+info_encoded_str=buffer.getvalue()
+
+file_name_encoded_info='df_encoded_info_output.txt'
+
+full_file_path_encoded_info=os.path.join(output_folder,file_name_encoded_info)
+
+with open(full_file_path_encoded_info,'w') as f:
+    f.write(info_encoded_str)
+
+# df_encoded.head - What does a small sample of the data look like? -- I have also used this function to test if the file has been loaded successfully
+df_head_encoded_str=df_encoded.head(500).to_string(index=False)
+
+file_path_encoded_head="df_encoded_head_output.txt"
+
+full_file_path_encoded_head=os.path.join(output_folder,file_path_encoded_head)
+
+with open(full_file_path_encoded_head, 'w') as f:
+    f.write(df_head_encoded_str)
+
+print(df_encoded)
+
